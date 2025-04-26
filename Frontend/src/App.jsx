43d2dff51df2b679d2acf845da006fcd43b846e3
amrit <amritkumarverma848@gmail.com>
2025-04-26 21:36:45 +0530

    Initializing the project with all necessary Node modules

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,8 @@
+import Home from "./Components/Home"
+import Login from "./Components/Login"
+import Upload from "./Components/Upload"
+import Profile from "./Components/Profile"
+import {BrowserRouter, Route, Routes} from 'react-router'
 
 
 function App() {
@@ -9,8 +14,10 @@ function App() {
      
      <Routes>
 
-        <Route path='/' element={<DashBoard />} />
-
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/upload' element={<Upload />} />
+        <Route path='/profile' element={<Profile />} />
      </Routes>
 
      </BrowserRouter>
